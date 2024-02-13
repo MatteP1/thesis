42dab7f0f6866d513eb4f2157f83be8c50cd02f8
RusTy <mathiasp9999@gmail.com>
2024-02-13 03:40:45 +0100

    added file for two-lock msqueue

---
+++ iris/twoLockMSQ.v
@@ -0,0 +1,8 @@
+From iris.heap_lang Require Import lang proofmode notation.
+
+Context `{!heapGS Σ}.
+
+(* TODO: implement *)
+Definition initialize : expr := #().
+Definition enqueue : expr := #().
+Definition dequeue : expr := #().
