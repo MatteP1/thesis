2151cc30bbb3962bdbdb2dbf6badd23fc8842065
RusTy <mathiasp9999@gmail.com>
2024-02-14 19:04:44 +0100

    added tests and fixed a couple of bugs in implementation.

--- theories/twoLockMSQ.v
+++ theories/twoLockMSQ.v
@@ -1,5 +1,6 @@
 From iris.heap_lang Require Import lang proofmode notation.
 From iris.heap_lang.lib Require Import lock spin_lock.
+From iris.unstable.heap_lang Require Import interpreter.
 
 Local Existing Instance spin_lock.
 
@@ -21,7 +22,7 @@ Definition enqueue : val :=
 	rec: "enqueue" "Q" "value" :=
 		let: "node" := ref (SOME (SOME "value", ref(ref(NONE)))) in
 		acquire (Snd ( Snd (!"Q")));; (* Acqurie T_lock *)
-		Snd ("get_some" (!(Snd (Fst(!"Q"))))) <- "node" ;;
+		Snd (get_some !(!(Snd (Fst(!"Q"))))) <- "node" ;;
 		Snd (Fst (!"Q")) <- "node" ;;
 		release (Snd (Snd (!"Q"))).
 
@@ -29,17 +30,60 @@ Definition dequeue : val :=
 	rec: "dequeue" "Q" := 
 		acquire (Fst (Snd (!"Q")));; (* Acquire H_lock *)
 		let: "node" := !(Fst (Fst (!"Q"))) in (* Get Head node *)
-		let: "new_head" := !(Snd("get_some" (!"node"))) in (* Find Head.Next *)
+		let: "new_head" := !(Snd(get_some (!"node"))) in (* Find Head.Next *)
 		if: !"new_head" = NONE then (* Check if Queue is empty *)
 			(* No Next node. Queue is empty. *)
 			release (Fst (Snd(!"Q"))) ;;
 			NONEV
 		else
 			(* Queue not empty. Pop first element in Queue *)
-			let: "value" := Fst (!"new_head") in (* Get its value *)
-			Fst (Fst (!"Q")) <- "new_head" (* Swing Head to next node *)
-			release (Fst (Snd (!"Q")));; (* Release H_lock *)
+			let: "value" := Fst (get_some (!"new_head")) in (* Get its value *)
+			Fst (Fst (!"Q")) <- "new_head" ;; (* Swing Head to next node *)
+			release (Fst (Snd (!"Q"))) ;; (* Release H_lock *)
 			"value". (* Return value *)
+
+Section tests.
+
+Definition test_initialize : expr :=
+	let: "Q" := initialize #() in
+	"Q".
+
+Definition test_enqueue : expr :=
+	let: "Q" := initialize #() in
+	enqueue "Q" #5 ;;
+	enqueue "Q" #7 ;;
+	#().
+
+Definition test_dequeue1 : expr :=
+	let: "Q" := initialize #() in
+	enqueue "Q" #5 ;;
+	let: "v1" := dequeue "Q" in
+	"v1".
+
+Definition test_dequeue2 : expr :=
+	let: "Q" := initialize #() in
+	enqueue "Q" #5 ;;
+	enqueue "Q" #7 ;;
+	let: "v1" := dequeue "Q" in
+	let: "v2" := dequeue "Q" in
+	("v1", "v2").
+
+Definition test_dequeue_empty1 : expr :=
+	let: "Q" := initialize #() in
+	let: "v1" := dequeue "Q" in
+	"v1".
+
+Definition test_dequeue_empty2 : expr :=
+	let: "Q" := initialize #() in
+	enqueue "Q" #5 ;;
+	let: "v1" := dequeue "Q" in
+	let: "v2" := dequeue "Q" in
+	let: "v3" := dequeue "Q" in
+	("v1", "v2", "v3").
+
+Compute (exec 200 test_dequeue_empty2).
+
+End tests.
 
 Section proofs.
 
